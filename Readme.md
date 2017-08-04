da0bcfdf0d5bf06f4441b562a774161759b55b2b
式燕 <wenjian.ywj@alibaba-inc.com>
2017-08-04 20:34:38 +0800

    init

---
+++ Readme.md
@@ -0,0 +1,43 @@
+# Readme
+
+### 步骤
+
+* 全局安装 karma
+
+```
+npm install -g karma-cli
+```
+
+* Fork 代码仓库并拉到本地
+* 安装所有依赖
+
+```
+npm install
+```
+
+* 初始化测试
+
+```
+karma init
+```
+    1. Which testing framework do you want to use ? (mocha)
+    2. Do you want to use Require.js ? (no)
+    3. Do you want to capture any browsers automatically ? (Chrome)
+    4. What is the location of your source and test files ? (https://cdn.bootcss.com/jquery/2.2.4/jquery.js, node_modules/should/should.js, test/**.js)
+    5. Should any of the files included by the previous patterns be excluded ? ()
+    6. Do you want Karma to watch all the files and run the tests on change ? (yes)
+
+* 启动测试
+
+```
+karma start
+```
+
+* 打开 `./test/test.js` 修改 `./lib/add.js` 跑通测试用例 
+
+* 提交github，尝试接入Travis CI
+
+### 参考
+
+* [karma](http://karma-runner.github.io/)
+* [jQuery 中文文档](http://jquery.cuishifeng.cn/)
---
+++ test/test.js
@@ -0,0 +1,36 @@
+describe('jQuery', function () {
+  it('should have jQuery', function () {
+    if (!window.jQuery) {
+      throw new Error('查看下 karma.conf.js 配置项 files 是否正确')
+    }
+  })
+
+  it('should able to get a body', function () {
+    var $body = $('body')
+    $body.length.should.equal(1)
+    $body[0].should.equal(document.getElementsByTagName('body')[0])
+  })
+
+  describe('should able to trigger an event', function () {
+    var ele
+    before(function () {
+      ele = document.createElement('button')
+      document.body.appendChild(ele)
+    })
+
+    it('should able trigger an event', function (done) {
+      $(ele).on('click', function () {
+        done()
+      }).trigger('click')
+    })
+
+    after(function () {
+      document.body.removeChild(ele)
+      ele = null
+    })
+  })
+
+  it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (done) {
+    // 使用 jQuery.ajax 请求 https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js，并验证是否拿到文件
+  })
+})
