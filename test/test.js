9d3597b121ea1b410be6443ffa1147ae2a64e2f5
JaffarL <lyzandwow@163.com>
2018-01-25 04:49:13 -0800

    done

--- test/test.js
+++ test/test.js
@@ -30,7 +30,15 @@ describe('jQuery', function () {
     })
   })
 
-  it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (done) {
+  it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', 
+    function (done) {
+      $.ajax({
+        type:'GET',
+        url:'https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js',
+        success:function(data){
+          done();
+        }
+      })
     // 使用 jQuery.ajax 请求 https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js，并验证是否拿到文件
   })
 })
