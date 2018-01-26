d058014183311567b20ad4672499f9be75759ed1
tedlife <zhongguojym@gmail.com>
2018-01-26 23:21:57 +0800

    remove: chrome

--- karma.conf.js
+++ karma.conf.js
@@ -44,15 +44,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ["Chrome", "Firefox"],
-
-    // travis cli 使用 chrome 测试
-    customLaunchers: {
-      Chrome_travis_ci: {
-        base: "Chrome",
-        flags: ["--no-sandbox"]
-      }
-    },
+    browsers: ["Firefox"],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
