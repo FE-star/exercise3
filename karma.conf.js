dcf954d1fda415a9ad6e3a5ed0613c3d7942814e
Wang Yuanli <wangyuanlitin@gmail.com>
2018-01-27 18:33:55 +0800

    karma single run

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
+    singleRun: true,
     concurrency: Infinity
   }
   if (process.env.TRAVIS) {
