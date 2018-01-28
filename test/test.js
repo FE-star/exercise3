7f6c8b2cb24ef6575cbb711542c5b588cda52650
null-br <wanglu1@yy.com>
2018-01-28 23:24:10 +0800

    push

--- test/test.js
+++ test/test.js
@@ -32,5 +32,17 @@ describe('jQuery', function () {
 
   it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (done) {
     // 使用 jQuery.ajax 请求 https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js，并验证是否拿到文件
+    $.ajax({
+      url:
+        "https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js",
+      success: res => {
+        // https://shouldjs.github.io/#should-exist
+        should.exist(res);
+        done();
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
   })
 })
