859c81251cfa5faaab07833d8f4be661fa376167
劳铱婷 <eating_miao@163.com>
2018-01-28 12:15:06 +0800

    eating's exercise3

--- test/test.js
+++ test/test.js
@@ -32,5 +32,16 @@ describe('jQuery', function () {
 
   it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (done) {
     // 使用 jQuery.ajax 请求 https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js，并验证是否拿到文件
+    $.ajax({
+       url: 'https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js',
+       type: 'GET',
+       success: (data) => {
+         should.exist(data)
+         done()
+       },
+       error: (XMLHttpRequest, status, error)  => {
+         console.log(error);
+       }
+     })
   })
 })
