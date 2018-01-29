eb079790970069377d333d4175a76ace04356612
shen <357789504@qq.com>
2018-01-29 16:54:27 +0800

    add():通过测试

--- test/test.js
+++ test/test.js
@@ -3,34 +3,41 @@ describe('jQuery', function () {
     if (!window.jQuery) {
       throw new Error('查看下 karma.conf.js 配置项 files 是否正确')
     }
-  })
+  });
 
   it('should able to get a body', function () {
-    var $body = $('body')
-    $body.length.should.equal(1)
+    var $body = $('body');
+    $body.length.should.equal(1);
     $body[0].should.equal(document.getElementsByTagName('body')[0])
-  })
+  });
 
   describe('should able to trigger an event', function () {
-    var ele
+    var ele;
     before(function () {
-      ele = document.createElement('button')
+      ele = document.createElement('button');
       document.body.appendChild(ele)
-    })
+    });
 
     it('should able trigger an event', function (done) {
       $(ele).on('click', function () {
         done()
       }).trigger('click')
-    })
+    });
 
     after(function () {
-      document.body.removeChild(ele)
+      document.body.removeChild(ele);
       ele = null
     })
-  })
+  });
 
   it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (done) {
     // 使用 jQuery.ajax 请求 https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js，并验证是否拿到文件
+    $.ajax({
+      url: 'https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js',
+      success: function (result) {
+        should.exist(result);
+        done()
+      }
+    });
   })
-})
+});
