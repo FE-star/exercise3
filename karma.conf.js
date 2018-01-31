d1f0da6a2f669a963041c7d79d09187127afe9ef
王建忠 <yepbug@qq.com>
2018-01-31 11:13:05 +0800

    update

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,14 @@
 
 module.exports = function(config) {
   config.set({
-
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    // you can define custom flags
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
