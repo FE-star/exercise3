9ca5d6e8aff21ea77284bfc0198f4faf99f6c4a4
侯先诚 <imhxc@houxianchengdeMacBook-Pro.local>
2018-01-31 13:21:38 +0800

    #

--- karma.conf.js
+++ karma.conf.js
@@ -1,78 +1,71 @@
 // Karma configuration
-// Generated on Wed Jan 24 2018 23:19:33 GMT+0800 (CST)
+// Generated on Fri Jan 26 2018 21:47:03 GMT+0800 (CST)
 
-module.exports = function(config) {
-  config.set({
-
+module.exports = function (config) {
+  var configuration = {
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
+    basePath: "",
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
-
+    frameworks: ["mocha"],
 
     // list of files / patterns to load in the browser
     files: [
-      'https://cdn.bootcss.com/jquery/2.2.4/jquery.js',
-      'node_modules/should/should.js',
-      'test/**.js'
+      "https://cdn.bootcss.com/jquery/2.2.4/jquery.js",
+      "node_modules/should/should.js",
+      "test/**.js"
     ],
 
-
-    // list of files to exclude
-    exclude: [
-    ],
-
+    // list of files / patterns to exclude
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
+    preprocessors: {},
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
+    reporters: ["progress"],
 
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    // browsers: ['Chrome'],
-    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    browsers: ["Chrome", "Firefox"],
 
+    // travis cli 使用 chrome 测试
+    // https://docs.travis-ci.com/user/chrome 这里有说明为什么谷歌需要加--no-sandbox这个
     customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+      Chrome_travis_ci: {
+        base: "Chrome",
+        flags: ["--no-sandbox"]
       }
     },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: process.env.TRAVIS,
 
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity
-  })
-}
+  };
+  // https://swizec.com/blog/how-to-run-javascript-tests-in-chrome-on-travis/swizec/6647
+  if (process.env.TRAVIS) {
+    // configuration.browsers = ["Chrome_travis_ci", "Firefox"];
+  }
+  config.set(configuration);
+};
