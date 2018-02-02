ef2efc4ef37fb258c153fd91d6a4bd4cb2ff774e
wanglei-elf <303736135@qq.com>
2018-02-02 09:32:43 +0800

    Update karma.config

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@
 // Generated on Wed Jan 31 2018 22:49:41 GMT+0800 (CST)
 
 module.exports = function(config) {
-  config.set({
+  let cfg = {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
@@ -58,10 +58,10 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
 
     customLaunchers: {
-      Chrome_travis_ci: {
+      ChromeHeadlessNoSandbox: {
         base: 'Chrome',
         flags: ['--no-sandbox']
       }
@@ -75,5 +75,8 @@ module.exports = function(config) {
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity
-  })
+  }
+  if(process.env.TRAVIS){
+    cfg.browsers=['ChromeHeadlessNoSandbox'];
+  }
 }
