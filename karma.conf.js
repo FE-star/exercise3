ca831b984b3be73c80d35bb0ee67271c0992e8d1
王蕾 <wanglei@wangleideMBP.lan>
2018-02-02 21:16:17 +0800

    update .travis.yml

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,6 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/requirejs/require.js',
       'https://cdn.bootcss.com/jquery/2.2.4/jquery.js',
       'node_modules/should/should.js',
       'test/test.js'
