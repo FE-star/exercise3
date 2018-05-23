3057284bb764bbb7003833af4db9638ad505039a
Chenjiayuan195 <30591313+Chenjiayuan195@users.noreply.github.com>
2018-05-24 00:48:30 +0800

    更新作业

--- karma.conf.js
+++ karma.conf.js
@@ -66,6 +66,8 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+    
+    browserNoActivityTimeout: 30000
   })
 }
