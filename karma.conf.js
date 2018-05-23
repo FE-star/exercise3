1cd1f41658330b2c2efeff7a0dead847f549808d
Chenjiayuan195 <30591313+Chenjiayuan195@users.noreply.github.com>
2018-05-24 02:30:40 +0800

    提交代码

--- karma.conf.js
+++ karma.conf.js
@@ -57,7 +57,8 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'，'Firefox'],
+    browsers: ['Chrome',
+    'Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
