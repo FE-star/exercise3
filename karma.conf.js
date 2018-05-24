52ac061d4912f9f9eb7fa826bdb39cb30d2ec968
Chenjiayuan195 <30591313+Chenjiayuan195@users.noreply.github.com>
2018-05-24 23:00:35 +0800

    提交代码

--- karma.conf.js
+++ karma.conf.js
@@ -57,12 +57,16 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Chrome','Chrome_without_security','ChromeHeadless', 'ChromeHeadlessNoSandbox'],
     customLaunchers: {
+        Chrome_without_security: {
+            base: 'Chrome',
+            flags: ['--disable-web-security']
+         }
          ChromeHeadlessNoSandbox: {
            base: 'ChromeHeadless',
            flags: ['--no-sandbox']
-         }
+         },
        },
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
