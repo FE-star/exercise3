4dab35b90be8789a843c83e15ae910662d84de45
fridolph <249121486@qq.com>
2018-05-26 20:22:16 +0800

    更改配置文件

--- karma.conf.js
+++ karma.conf.js
@@ -54,6 +54,12 @@ module.exports = function(config) {
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
+    plugins: [
+      'karma-mocha',
+      'karma-chrome-launcher',
+    ],
+
+
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
