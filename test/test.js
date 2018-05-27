643e64e6c92d77bdbda76047c918a5c79da427fa
DDLancer5 <694730359@qq.com>
2018-05-27 23:20:44 +0800

    feat: complate exercise3

--- test/test.js
+++ test/test.js
@@ -32,5 +32,16 @@ describe('jQuery', function () {
 
   it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (done) {
     // 使用 jQuery.ajax 请求 https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js，并验证是否拿到文件
+      $.ajax({
+        url: 'https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js',
+        method: 'GET',
+        success(res) {
+          console.log(res);
+          done();
+        },
+        error(err) {
+          console.error(err);
+        }
+      })
   })
 })
