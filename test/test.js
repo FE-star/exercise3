cf2a0739a1ef5419b5b72a0241ab9a5791100eba
karisluo <karisluo@ifastfinancial.com.cn>
2018-05-28 19:16:08 +0800

    添加配置文件，添加karma

--- test/test.js
+++ test/test.js
@@ -30,7 +30,18 @@ describe('jQuery', function () {
     })
   })
 
-  it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (done) {
-    // 使用 jQuery.ajax 请求 https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js，并验证是否拿到文件
+  it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (response) {
+    $.ajax({
+         url: 'https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js',
+          method: 'GET',
+          success(res) {
+           console.log('success\n', res)
+           cb()
+           },
+            error(err) {
+              console.error(err)
+            }
+          })
+         })
   })
 })
