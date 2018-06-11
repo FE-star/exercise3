f05573fb928b3dfe053ec95089d7b8f58ee47dab
yuxin <yx18513839223@gmail.com>
2018-06-11 23:02:30 +0800

    finish

--- test/test.js
+++ test/test.js
@@ -31,6 +31,15 @@ describe('jQuery', function () {
   })
 
   it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (done) {
-    // 使用 jQuery.ajax 请求 https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js，并验证是否拿到文件
+    // 使用 jQuery.ajax 请求 https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js，
+    // 并验证是否拿到文件
+    $.ajax({
+        type: 'get',
+        url: 'https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js',
+        success: function(data) {
+            data.should.startWith("var assert = require('assert')");
+            done();
+        }
+    });
   })
 })
