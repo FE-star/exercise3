ba98736e802928abef3a60b4cfac95f8b20cd2d5
yujiayu <yujiayu@lizhi.fm>
2018-06-11 16:25:49 +0800

    no message

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 describe('jQuery', function () {
   it('should have jQuery', function () {
+    console.log($)
     if (!window.jQuery) {
       throw new Error('查看下 karma.conf.js 配置项 files 是否正确')
     }
@@ -20,6 +21,7 @@ describe('jQuery', function () {
 
     it('should able trigger an event', function (done) {
       $(ele).on('click', function () {
+        alert(231)
         done()
       }).trigger('click')
     })
@@ -32,5 +34,10 @@ describe('jQuery', function () {
 
   it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (done) {
     // 使用 jQuery.ajax 请求 https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js，并验证是否拿到文件
+    $.ajax({url: "https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js", success:function(result) {
+      console.log(result)
+      $("body").html(result);
+      done()
+    }});
   })
 })
