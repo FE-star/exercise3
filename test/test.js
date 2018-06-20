f984c3aa1f2fda641ad5b1130e73111e994eba5a
Little Rui <1043448355@qq.com>
2018-06-20 18:03:02 +0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -33,7 +33,7 @@ describe('jQuery', function () {
   it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (done) {
     // 使用 jQuery.ajax 请求 https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js，并验证是否拿到文件
     $.ajax({
-      url: "https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.s",
+      url: "https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js",
       type: 'GET'
       // success: function(data){
       //   done();
