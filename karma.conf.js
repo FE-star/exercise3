024be04ba01ad93faeeaa5e32aa85da61ba2cbb8
zhangcan <18825285238@163.com>
2018-06-23 22:54:28 +0800

    [config] fix karma.config to pass travis CI

--- karma.conf.js
+++ karma.conf.js
@@ -57,7 +57,16 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeHeadless'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+
+
+    // you can define custom flags，otherwise you can pass travis CI:cannot start chromeheadless
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
 
 
     // Continuous Integration mode
