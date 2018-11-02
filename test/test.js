736eb97de11a09015ce8dd30691d16a550ce9999
yanjian <yanjian@yanjian-pc.local>
2018-11-02 23:07:06 +0800

    test travis ci

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 describe('jQuery', function () {
   it('should have jQuery', function () {
     if (!window.jQuery) {
-      throw new Error('查看下 karma.conf.js 配置项 files 是否正确')
+      throw new Error('查看下 karma.conf.js 配置项 files 是否正确!!')
     }
   })
 
