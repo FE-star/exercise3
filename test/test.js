1021c324390e633bb7e5438051f2866a004409d3
chenyu <iambigchen@163.com>
2018-11-02 22:36:11 +0800

    test

--- test/test.js
+++ test/test.js
@@ -17,9 +17,6 @@ describe('jQuery', function () {
       ele = document.createElement('button')
       document.body.appendChild(ele)
     })
-    it('test for Travis CI', function() {
-      should(2).equal(11)
-    })
     it('should able trigger an event', function (done) {
       $(ele).on('click', function () {
         done()
