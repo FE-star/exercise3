87a51caf831b2fe7e4b3aa875c3fbc54d69b6f70
wangshuang0 <wlwangshuangshuang@163.com>
2018-11-03 21:42:55 +0800

    exercise3 modify

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'https://cdn.bootcss.com/jquery/2.2.4/jquery.js, node_modules/should/should.js, test/**.js'
+      'https://cdn.bootcss.com/jquery/2.2.4/jquery.js', 'node_modules/should/should.js', 'test/**.js'
     ],
 
 
