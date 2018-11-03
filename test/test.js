444df58b9a9beae0ad8e6353c07bf8c980caf8b0
zhaoyan123456789 <1269894710@qq.com>
2018-11-03 16:04:26 +0800

    删除测试代码

--- test/test.js
+++ test/test.js
@@ -2,7 +2,6 @@ describe('jQuery', function () {
     it('should have jQuery', function () {
         if (!window.jQuery) {
             throw new Error('查看下 karma.conf.js 配置项 files 是否正确')
-            console.log('测试webstorm上传')
         }
     })
 
