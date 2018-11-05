e4b9afb359943cbccacdc2402b1880eaa70da4d9
Admin <admin@Admins-MacBook.local>
2018-11-05 14:25:55 +0800

    beautify test.js

--- test/test.js
+++ test/test.js
@@ -1,43 +1,43 @@
-describe('jQuery', function () {
-  it('should have jQuery', function () {
+describe('jQuery', function() {
+  it('should have jQuery', function() {
     if (!window.jQuery) {
       throw new Error('查看下 karma.conf.js 配置项 files 是否正确')
     }
   })
 
-  it('should able to get a body', function () {
+  it('should able to get a body', function() {
     var $body = $('body')
     $body.length.should.equal(1)
     $body[0].should.equal(document.getElementsByTagName('body')[0])
   })
 
-  describe('should able to trigger an event', function () {
+  describe('should able to trigger an event', function() {
     var ele
-    before(function () {
+    before(function() {
       ele = document.createElement('button')
       document.body.appendChild(ele)
     })
 
-    it('should able trigger an event', function (done) {
-      $(ele).on('click', function () {
+    it('should able trigger an event', function(done) {
+      $(ele).on('click', function() {
         done()
       }).trigger('click')
     })
 
-    after(function () {
+    after(function() {
       document.body.removeChild(ele)
       ele = null
     })
   })
 
-  it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (done) {
+  it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function(done) {
     // 使用 jQuery.ajax 请求 https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js，并验证是否拿到文件
     var url = 'https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js'
     $.ajax({
       url: 'https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js',
       type: 'GET',
-      success: function (res) {
-        should.exist(res); 
+      success: function(res) {
+        should.exist(res);
         done()
       }
     })
