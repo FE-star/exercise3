6a3c34f4116f929a4f998c6dd6ff6e5f22febc39
Zeshi Zheng <mailtozeshi@gmail.com>
2019-03-13 20:05:54 -0400

    set up chrome for CI

--- karma.conf.js
+++ karma.conf.js
@@ -2,70 +2,83 @@
 // Generated on Wed Mar 13 2019 18:15:34 GMT-0400 (Eastern Daylight Time)
 
 module.exports = function(config) {
-  config.set({
+  const conditionalConfig = {
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+	  // base path that will be used to resolve all patterns (eg. files, exclude)
+	  basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-      'https://cdn.bootcss.com/jquery/2.2.4/jquery.js',
-      'node_modules/should/should.js',
-      'test/**.js'
-    ],
+	  // frameworks to use
+	  // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+	  frameworks: ['mocha'],
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+	  // list of files / patterns to load in the browser
+	  files: [
+		  'https://cdn.bootcss.com/jquery/2.2.4/jquery.js',
+		  'node_modules/should/should.js',
+		  'test/**.js'
+	  ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+	  // list of files to exclude
+	  exclude: [
+	  ],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+	  // preprocess matching files before serving them to the browser
+	  // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+	  preprocessors: {
+	  },
 
 
-    // web server port
-    port: 9876,
+	  // test results reporter to use
+	  // possible values: 'dots', 'progress'
+	  // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+	  reporters: ['progress'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+	  // web server port
+	  port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+	  // enable / disable colors in the output (reporters and logs)
+	  colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+	  // level of logging
+	  // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+	  logLevel: config.LOG_INFO,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+	  // enable / disable watching file and executing tests whenever any file changes
+	  autoWatch: true,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+	  // start these browsers
+	  // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+	  browsers: ['Chrome'],
+	  customLaunchers: { // special setup for travis karma test
+		  Chrome_travis_ci: {
+			  base: 'Chrome',
+			  flags: ['--no-sandbox']
+		  }
+	  },
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
+	  // Continuous Integration mode
+	  // if true, Karma captures browsers, runs the tests and exits
+	  singleRun: false,
+
+	  // Concurrency level
+	  // how many browser should be started simultaneous
+	  concurrency: Infinity
+  }
+
+  // set up chrome for travis karma test
+  if (process.env.TRAVIS) {
+	  conditionalConfig.browsers = ['Chrome_travis_ci'];
+	  conditionalConfig.singleRun = true; // exit after a npm test, otherwise travis won't npm run github
+  }
+
+  config.set(conditionalConfig);
 }
