f105771ce0509c69dc0f5c86f9dc0cbc51341bf2
panqiang <534047750@qq.com>
2019-03-14 14:49:17 +0800

    欲哭无泪

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
+
 describe('jQuery', function () {
+
   it('should have jQuery', function () {
     if (!window.jQuery) {
       throw new Error('查看下 karma.conf.js 配置项 files 是否正确')
@@ -32,5 +34,14 @@ describe('jQuery', function () {
 
   it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (done) {
     // 使用 jQuery.ajax 请求 https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js，并验证是否拿到文件
+    $.ajax({
+      url: "https://raw.githubusercontent.com/FE-star/exercise2/master/test/test.js",
+      success: () => {
+        done()
+      },
+      error: () => {
+        done()
+      }
+    });
   })
 })
