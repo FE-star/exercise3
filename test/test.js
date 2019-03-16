38d11fac5346d421177ddf5c93ba9f19f7fd160b
FreeWisdom <1134941536@qq.com>
2019-03-16 21:06:40 +0800

    feat：测试通过，jQuery.ajax拿到文件失败

--- test/test.js
+++ test/test.js
@@ -32,5 +32,20 @@ describe('jQuery', function () {
 
   it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (done) {
     // 使用 jQuery.ajax 请求 https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js，并验证是否拿到文件
+    console.log(jQuery.ajax, jQuery.getScript, window.jQuery.getScript, $,);
+    jQuery.getScript("https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js", function() {
+      console.log("载入完成")
+    });
+
+    jQuery.ajax({
+      type:"GET",
+      url:"./data/shuju.json",
+      dataType: "script",
+      success: function(data) {
+        console.log(data)
+      }
+    });
+
+    done();
   })
 })
