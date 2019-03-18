78691e01a49b03d15b7fe5ac4501596ac9a63769
tang119 <1296724559@qq.com>
2019-03-18 15:21:03 +0800

    try travis ci

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 describe('jQuery', function () {
   it('should have jQuery', function () {
+    console.log(window.jQuery)
     if (!window.jQuery) {
       throw new Error('查看下 karma.conf.js 配置项 files 是否正确')
     }
@@ -32,5 +33,12 @@ describe('jQuery', function () {
 
   it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (done) {
     // 使用 jQuery.ajax 请求 https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js，并验证是否拿到文件
+      jQuery.ajax({
+          url:'https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js',
+          method: 'GET',
+          success:function () {
+              done()
+          }
+      })
   })
 })
