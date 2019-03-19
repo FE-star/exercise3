8e14388fb6a4fa879ed94bc68ec275a67a42c3d8
cj348986512 <348986512@qq.com>
2019-03-19 14:42:22 +0800

    修改配置集成到travis

--- karma.conf.js
+++ karma.conf.js
@@ -52,12 +52,12 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    autoWatch: false,
 
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeCanary'],
 
 
     // Continuous Integration mode
